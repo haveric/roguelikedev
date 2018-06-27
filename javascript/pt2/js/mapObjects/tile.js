166e5c4223cbbaf553e6e8a3edefc243cc4bc42b
Ryan Breuer <breuerryan@gmail.com>
2018-06-27 01:25:47 -0500

    Part 2 in JavaScript

---
+++ javascript/pt2/js/mapObjects/tile.js
@@ -0,0 +1,9 @@
+var Tile = function(blocked, blockSight = false) {
+	this.blocked = blocked;
+	this.blockSight = blocked || blockSight;
+}
+
+Tile.prototype.setBlocked = function() {
+	this.blocked = true;
+	this.blockSight = true;
+}
